6e5db0160e1d6310bf614a63628dce035a8f11e1
Jose Leonardo Quiñones <desarrollojlq@outlook.es>
2025-05-17 13:43:47 -0500

    asyc Task.whenAll

--- Program.cs
+++ Program.cs
@@ -1,13 +1,37 @@
-﻿class Program
+﻿public class Program
 {
-    static void Main(string[] args)
+    public async Task DownloadDataAsync()
     {
-        Person person1 = new Person("Alice", 30);
-        Person person2 = new Person("Bob", 25);
-        Person person3 = new Person("Leo", 26);
-
-        person1.Greet();
-        person2.Greet();
-        person3.Greet();
+        try
+        {
+            Console.WriteLine("Download started...");
+            throw new InvalidOperationException("Simulated download error.");
+            await Task.Delay(3000);
+            Console.WriteLine("Download completed.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
     }
+    public async Task DownloadDataAsync2()
+    {
+        Console.WriteLine("Download 2 started...");
+        await Task.Delay(2000);
+        Console.WriteLine("Download 2 completed.");
+    }
+
+    public static async Task Main(string[] args)
+    {
+        Program program = new Program();
+        Task task1 = program.DownloadDataAsync();
+        Task task2 = program.DownloadDataAsync2();
+        await Task.WhenAll(task1, task2);
+        // await program.DownloadDataAsync();
+        // await program.DownloadDataAsync2();
+        Console.WriteLine("Main completed");
+
+    }
+
+
 }
